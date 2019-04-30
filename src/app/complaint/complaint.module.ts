9bea8a22d90ca52d7fcf3fbcca28757cdf4532a8
szuperaz <szuperaz@gmail.com>
2019-04-30 17:25:46 +0200

    initial commit

---
+++ src/app/complaint/complaint.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComplaintRoutingModule } from './complaint-routing.module';
+import { ComplaintComponent } from './complaint/complaint.component';
+import { ComplaintListComponent } from './complaint-list/complaint-list.component';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [ComplaintComponent, ComplaintListComponent],
+  imports: [
+    CommonModule,
+    ComplaintRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+  ]
+})
+export class ComplaintModule {}
