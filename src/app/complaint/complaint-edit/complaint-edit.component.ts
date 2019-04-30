311147573dda0fd9aa255ddf40c9df666e5467d3
szuperaz <szuperaz@gmail.com>
2019-04-30 18:39:51 +0200

    complaints can be edited

--- src/app/complaint/complaint-edit/complaint-edit.component.ts
+++ src/app/complaint/complaint-edit/complaint-edit.component.ts
@@ -3,6 +3,9 @@ import { NewComplaint } from '../new-complaint';
 import { NgForm } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { ComplaintService } from '../complaint.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'szia-complaint-edit',
@@ -10,25 +13,45 @@ import { ComplaintService } from '../complaint.service';
   styleUrls: ['./complaint-edit.component.scss']
 })
 export class ComplaintEditComponent implements OnInit {
-  complaint = new NewComplaint();
+  complaint;
   urlValidationPattern =
     '^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&\'\\(\\)\\*\\+,;=.]+$';
   @ViewChild('form') private form: NgForm;
   isLoading = false;
+  private isNew: boolean;
+  private complaintId: number;
 
-  constructor(private complaintService: ComplaintService, private snackBar: MatSnackBar) { }
+  constructor(
+    private complaintService: ComplaintService,
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.complaint.name = 'Teszt';
+    this.route.queryParams.pipe(switchMap((params) => {
+      if (params['id']) {
+        this.isNew = false;
+        this.complaintId = +params['id'];
+        return this.complaintService.getComplaint(this.complaintId);
+      } else {
+        this.isNew = true;
+        return of(new NewComplaint());
+      }
+    })).subscribe(complaint => this.complaint = complaint);
   }
 
   submitted() {
     this.isLoading = true;
-    this.complaintService.saveComplaint(this.complaint).subscribe(() => {
+    const action = this.isNew ?
+      this.complaintService.saveComplaint(this.complaint) : this.complaintService.updateComplaint(this.complaintId, this.complaint);
+    action.subscribe(() => {
       this.isLoading = false;
       this.snackBar.open('Complaint saved', 'OK');
-      this.complaint = new NewComplaint();
       this.form.resetForm();
+      if (!this.isNew) {
+        this.router.navigate(['..'], {relativeTo: this.route});
+      }
     }, () => this.isLoading = false);
   }
 
